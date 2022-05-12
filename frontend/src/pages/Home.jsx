9954998131dc984ea6115fbdc7bbe6ddbea2579b
Kevin Willoughby <iamkevinn@protonmail.com>
2022-05-11 23:41:37 -0400

    Home, login, register pages

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,4 +1,22 @@
+import { Link } from 'react-router-dom';
+import { FaQuestionCircle, FaTicketAlt } from 'react-icons/fa';
+
 function Home() {
-  return <div>Home</div>;
+  return (
+    <>
+      <section className='heading'>
+        <h1>How can we assist you?</h1>
+        <p>Please choose from an option below.</p>
+      </section>
+
+      <Link to='/new-ticket' className='btn btn-reverse btn-block'>
+        <FaQuestionCircle /> Create New Ticket
+      </Link>
+
+      <Link to='/tickets' className='btn btn-block'>
+        <FaTicketAlt /> View My Tickets
+      </Link>
+    </>
+  );
 }
 export default Home;
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,4 +1,69 @@
+import { useState } from 'react';
+import { FaSignInAlt } from 'react-icons/fa';
+import { toast } from 'react-toastify';
+
 function Login() {
-  return <div>Login</div>;
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    password2: '',
+  });
+
+  const { name, email, password, password2 } = formData;
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <>
+      <section className='heading'>
+        <h1>
+          <FaSignInAlt /> Login
+        </h1>
+        <p>Please login in to access support</p>
+      </section>
+
+      <section className='form'>
+        <form onSubmit={onSubmit}>
+          <div className='form-group'>
+            <input
+              type='email'
+              className='form-control'
+              id='email'
+              name='email'
+              value={email}
+              onChange={onChange}
+              placeholder='Enter your email'
+              required
+            />
+          </div>
+          <div className='form-group'>
+            <input
+              type='password'
+              className='form-control'
+              id='password'
+              name='password'
+              value={password}
+              onChange={onChange}
+              placeholder='Enter a password'
+              required
+            />
+          </div>
+          <div className='form-group'>
+            <button className='btn btn-block'>Submit</button>
+          </div>
+        </form>
+      </section>
+    </>
+  );
 }
 export default Login;
