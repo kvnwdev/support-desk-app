8ab63714fa0058c59cbae8c608c0e946c9890b95
Kevin Willoughby <iamkevinn@protonmail.com>
2022-05-12 00:45:07 -0400

    Partial auth redux

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -1,4 +1,5 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import authService from './authService';
 
 const initialState = {
   user: null,
@@ -10,7 +11,19 @@ const initialState = {
 
 export const register = createAsyncThunk(
   'auth/register',
-  async (user, thunkAPI) => {}
+  async (user, thunkAPI) => {
+    try {
+      return await authService.register(user);
+    } catch (error) {
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString();
+      return thunkAPI.rejectWithValue(message);
+    }
+  }
 );
 
 export const login = createAsyncThunk(
@@ -22,7 +35,22 @@ export const authSlice = createSlice({
   name: 'auth',
   initialState,
   reducers: {},
-  extraReducers: (builder) => {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(register.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(register.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.user = action.payload;
+      })
+      .addCase(register.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.user = null;
+      });
+  },
 });
 
 export default authSlice.reducer;
