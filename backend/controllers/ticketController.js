85e6c72a07a3db261d09e89c60dd24c4a7080099
Kevin Willoughby <iamkevinn@protonmail.com>
2022-05-12 20:10:36 -0400

    Tickets put/del/get 1

--- backend/controllers/ticketController.js
+++ backend/controllers/ticketController.js
@@ -14,13 +14,44 @@ const getTickets = asyncHandler(async (req, res) => {
 
   if (!user) {
     res.status(401);
-    throw new Error('Unauthorized token');
+    throw new Error('Not authorized');
   }
 
   const tickets = await Ticket.find({ user: req.user.id });
 
   res.status(200).json({
     tickets,
+  });
+});
+
+/**
+ * @name getTicket
+ * @desc Fetch a single ticket
+ * @route GET /api/tickets/:id
+ * @access Private
+ */
+const getTicket = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user.id);
+
+  if (!user) {
+    res.status(401);
+    throw new Error('Unauthorized token');
+  }
+
+  const ticket = await Ticket.findById(req.params.id);
+
+  if (!ticket) {
+    res.status(404);
+    throw new Error('Ticket not found');
+  }
+
+  if (ticket.user.toString() !== req.user.id) {
+    res.status(401);
+    throw new Error('Not authorized');
+  }
+
+  res.status(200).json({
+    ticket,
   });
 });
 
@@ -56,7 +87,80 @@ const createTicket = asyncHandler(async (req, res) => {
   });
 });
 
+/**
+ * @name deleteTicket
+ * @desc Deletes a ticket
+ * @route DELETE /api/tickets/:id
+ * @access Private
+ */
+const deleteTicket = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user.id);
+
+  if (!user) {
+    res.status(401);
+    throw new Error('Unauthorized token');
+  }
+
+  const ticket = await Ticket.findById(req.params.id);
+
+  if (!ticket) {
+    res.status(404);
+    throw new Error('Ticket not found');
+  }
+
+  if (ticket.user.toString() !== req.user.id) {
+    res.status(401);
+    throw new Error('Not authorized');
+  }
+
+  await ticket.remove();
+
+  res.status(200).json({
+    success: true,
+  });
+});
+
+/**
+ * @name updateTicket
+ * @desc Updates a ticket
+ * @route PUT /api/tickets/:id
+ * @access Private
+ */
+const updateTicket = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user.id);
+
+  if (!user) {
+    res.status(401);
+    throw new Error('Unauthorized token');
+  }
+
+  const ticket = await Ticket.findById(req.params.id);
+
+  if (!ticket) {
+    res.status(404);
+    throw new Error('Ticket not found');
+  }
+
+  if (ticket.user.toString() !== req.user.id) {
+    res.status(401);
+    throw new Error('Not authorized');
+  }
+
+  const updatedTicket = await Ticket.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    { new: true }
+  );
+
+  res.status(200).json({
+    updatedTicket,
+  });
+});
+
 module.exports = {
   getTickets,
+  getTicket,
   createTicket,
+  deleteTicket,
+  updateTicket,
 };
